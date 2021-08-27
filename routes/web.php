3762382fc4e7d149bc861ea3558df41996596745
Raahim Fareed <raahim1965@hotmail.com>
2021-08-28 03:07:55 +0500

    Added routes for index, login, signup, home

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,29 @@
 <?php
 
+use App\Http\Controllers\Auth\RegisterController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Auth\LogoutController;
+use App\Http\Controllers\DefaultController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\NoteController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Str;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get("/", [DefaultController::class, "index"])->name("index");
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get("/register", [RegisterController::class, "index"])->name("register");
+Route::post("/register", [RegisterController::class, "store"]);
+
+Route::get("/login", [LoginController::class, "index"])->name("login");
+Route::post("/login", [LoginController::class, "store"]);
+
+Route::post("/logout", [LogoutController::class, "store"])->name("logout");
+
+Route::get("/home", [HomeController::class, "index"])->name("home");
+
+Route::post("/note/store", [NoteController::class, "store"])->name("note.store");
+Route::post("/note/update", [NoteController::class, "update"])->name("note.update");
+
+Route::get("/test", function () {
+    dd(Str::markdown("# Hello"));
 });
